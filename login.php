4ca710e12827275cdb03898c3b0998d91b05d381
KilianRn <66916233+KilianRn@users.noreply.github.com>
2022-03-07 11:49:01 +0100

    LOGIN PART

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    <?php
+      $user = $_POST['user'];
+      $user_password = $_POST['user_password'];
+      echo "user: " .$user ;
+      echo "<br>"; 
+      echo "password: " .$user_password;
+    ?>
+  </body>
+</html>
