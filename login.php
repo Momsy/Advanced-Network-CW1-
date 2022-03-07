48802c558155d022026aed085029326ab3f9ee27
KilianRn <66916233+KilianRn@users.noreply.github.com>
2022-03-07 18:59:22 +0100

    file storage

--- login.php
+++ login.php
@@ -3,9 +3,13 @@
     <?php
       $user = $_POST['user'];
       $user_password = $_POST['user_password'];
-      echo "user: " .$user ;
-      echo "<br>"; 
-      echo "password: " .$user_password;
+      //echo "user: " .$user ;
+      //echo "<br>"; 
+      //echo "password: " .$user_password;
+    $myfic = fopen('data.txt', 'w+');
+    fputs($myfic, $user);
+    fputs($myfic, $user_password);
+    fclose($myfic);
     ?>
   </body>
 </html>
